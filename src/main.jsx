cdc3c1695a3b0fdcbbd78531a1827d9f317826a4
imrane sarsri <imransarsi.ii@gmail.com>
2024-07-30 19:01:56 +0100

    save

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { ClockLoader } from 'react-spinners';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const RootComponent = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const handleLoad = () => {
-      setLoading(false);
-    };
-
-    window.addEventListener('load', handleLoad);
-
-    return () => {
-      window.removeEventListener('load', handleLoad);
-    };
-  }, []);
-
-  return (
-    <React.StrictMode>
-      {loading ? (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-          <ClockLoader color="#3ABEF9" size={200} />
-        </div>
-      ) : (
-        <App />
-      )}
-    </React.StrictMode>
-  );
-};
-
-ReactDOM.createRoot(document.getElementById('root')).render(<RootComponent />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
